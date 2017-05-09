bad7754a3aae69f3c94bb17c5b15346bd2bc627d
AAR <aar@AARs-MacBook-Pro.local>
2017-05-09 11:08:05 -0400

    getHearingThreshold bug

--- functions/getHearingThreshold.m
+++ functions/getHearingThreshold.m
@@ -6,7 +6,7 @@ hearingThreshold = zeros(1, N);
 
 % Calculate the Threshold for each bin freq
 for i = 1:N
-    freq = (i-1) * fs / N;
+    freq = (i-1) * 0.5 * fs / N;
     hearingThreshold(i) = 3.64*(freq^-0.8) - 6.5*exp(-0.6*((f-3.33)^2)) + 1e-3*(freq^4);
 end
 
